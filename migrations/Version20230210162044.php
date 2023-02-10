62f618436533da75e3eeb981af220ecc6144c5ca
Pierre Grimaud <grimaud.pierre@gmail.com>
2023-02-10 17:35:24 +0100

    Yo

---
+++ migrations/Version20230210162044.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230210162044 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE product_restaurant ADD stock INT NOT NULL, ADD price DOUBLE PRECISION NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE product_restaurant DROP stock, DROP price');
+    }
+}
